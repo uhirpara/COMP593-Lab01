d2608638fd133eecbd8d08f8cf9d7661dcfe8500
uhirpara <122479361+uhirpara@users.noreply.github.com>
2023-01-23 12:53:44 -0500

    put the comment back in bot the script

--- script2.py
+++ script2.py
@@ -1,4 +1,4 @@
-#print lines from zen of python
+#print lines from zen of python...
 print("Although never is often better than *right* now.")
 print("If the implementation is hard to explain, it's a bad idea.")
 print("If the implementation is easy to explain, it may be a good idea.")
