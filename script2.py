e4a70c904a18867803bd4b236b4a40b87eb55e5e
uhirpara <122479361+uhirpara@users.noreply.github.com>
2023-01-23 12:33:57 -0500

    Update script2.py

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
+#print lines from zen of python
 print("Although never is often better than *right* now.")
 print("If the implementation is hard to explain, it's a bad idea.")
 print("If the implementation is easy to explain, it may be a good idea.")
