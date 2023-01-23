1395fb53180f3422625c7ebba30b3573efd36ebe
uhirpara <122479361+uhirpara@users.noreply.github.com>
2023-01-23 12:29:25 -0500

    created script file
    Added code that prints lines from zen of python.

---
+++ script1.py
@@ -0,0 +1,2 @@
+print("Special cases aren't special enough to break the rules.")
+print("Now is better than never.")
---
+++ script2.py
@@ -0,0 +1,3 @@
+print("Although never is often better than *right* now.")
+print("If the implementation is hard to explain, it's a bad idea.")
+print("If the implementation is easy to explain, it may be a good idea.")
